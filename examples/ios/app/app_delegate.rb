815c873066ed14e3c3264462df49a8a2073489dc
Gant <GantMan@gmail.com>
2013-05-31 09:09:00 -0500

    Adding a friendly message to the example app

--- examples/ios/app/app_delegate.rb
+++ examples/ios/app/app_delegate.rb
@@ -1,6 +1,15 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @controller = UIViewController.alloc.initWithNibName(nil, bundle:nil)
+    @window.rootViewController = @controller
+    @window.makeKeyAndVisible
+
+    label = UILabel.new
+    label.text = "App Icon should be stamped: Close me and see!"
+    label.sizeToFit
+    label.center = @controller.view.frame.center
+    @controller.view.addSubview(label)
     true
   end
 end
